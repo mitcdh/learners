6d7ad0c14de9f75f89f55fb9cbe3e4fd1b0ba90c
git-lreuter <lenhard@reuter.graphics>
2021-11-18 15:26:23 +0100

    adapt initial loading of execution states

--- static/js/learners.js
+++ static/js/learners.js
@@ -1,30 +1,31 @@
 $(function () {
 
-    token = $(location).attr('search').split('?auth=')[1]
+    url_token = $(location).attr('search').split('?auth=')[1]
 
     // Set cookie if present in query-string
-    if (token) {
+    if (url_token) {
         // function definition in "token-handling.js"
-        setCookie('auth', token, 1)
+        setCookie('auth', url_token, 1)
     }
 
-
-    exercise_id = slugify($('.btn-run-exercise').first().attr('value'));
-    url_executeExercise = "http://127.0.0.1:5000/venjix/" + exercise_id
-    url_checkCompleted = "http://127.0.0.1:5000/venjix_status/" + exercise_id
+    // Get current token
     token = getCookie('auth')
-    
+
     indicator_id_executed = "#exercise_executed"
     indicator_id_completed = "#exercise_completed"
-    
-    executeAndCheck(
-        token, 
-        url_executeExercise, 
-        indicator_id_executed, 
-        url_checkCompleted, 
-        indicator_id_completed
-        )
 
+    script_name = $('.btn-run-exercise').first().attr('value')
+    if (script_name) {
+        script_name = slugify(script_name);
+        url_getCurrentState = "http://127.0.0.1:5000/current_state/" + script_name 
+        url_executeExercise = "http://127.0.0.1:5000/execute_script/" + script_name
+        // initial load
+        getCurrentState()
+    }
+    
+    url_checkCompleted = "http://127.0.0.1:5000/check_completion/"
+
+    
     // Run exercises
     $('.btn-run-exercise').click(function () {
 
