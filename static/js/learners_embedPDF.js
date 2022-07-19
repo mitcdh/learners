a5aab56ea955c1018072543f436b0001cdb45785
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2022-07-19 11:20:41 +0200

    Refactor pdf embedding

---
+++ static/js/learners_embedPDF.js
@@ -0,0 +1,31 @@
+function renderPDF(container_id, file) {
+  let pdfDoc = null;
+  let numPages = 0;
+
+  pdfjsLib.getDocument(file).promise.then((pdfDoc_) => {
+    pdfDoc = pdfDoc_;
+    numPages = Boolean($(`#${container_id}`).attr("show_all_pages"))
+      ? pdfDoc.numPages
+      : 1;
+    $(`#${container_id}`).attr("num_pages", numPages);
+
+    pdfDoc.getPage(1).then((page) => {
+      const viewport = page.getViewport({ scale: 1.5 });
+      $(`#${container_id}`).attr("ratio_height", viewport.height);
+      $(`#${container_id}`).attr("ratio_width", viewport.width);
+      resizePDF(container_id);
+    });
+  });
+}
+
+function resizePDF(container_id) {
+  numPages = $(`#${container_id}`).attr("num_pages");
+  ratio_height = $(`#${container_id}`).attr("ratio_height");
+  ratio_width = $(`#${container_id}`).attr("ratio_width");
+
+  const currWidth = $(`#${container_id}`).width();
+  let ratio = Math.floor(((ratio_height * numPages) / ratio_width) * 10) / 10;
+  $(`#${container_id}`).css({
+    height: currWidth * ratio + "px",
+  });
+}
