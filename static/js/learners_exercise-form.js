2aee5ffde68977c1569893362afdda7c88aa594f
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2023-04-11 10:23:37 +0200

    Add devstage attribute

--- static/js/learners_exercise-form.js
+++ static/js/learners_exercise-form.js
@@ -390,26 +390,36 @@ function getSectionValues(element) {
 }
 
 function callSetDrawIO(event, parent, url_encoded_data, button_element) {
-  const container = $(button_element).closest(".image-container");
+  const container = $(button_element).closest(".drawio-container");
   const current_input = $(container).find("textarea.drawio-input").val();
 
   if (current_input) url_encoded_data = current_input;
   url_encoded_data = url_encoded_data.replace("https://app.diagrams.net/", "");
 
-  try {
-    window.parent.postMessage({
-      'func': 'setDrawIO',
-      'message': `${url_encoded_data}`
-  }, "*");
+  console.log($(container).attr("devstage") === "true")
 
-    // parent.setDrawIO(url_encoded_data);
-  } catch (e) {
-    let newTab = window.open(
+  const openDrawioInNewTab = () => {
+    const newTab = window.open(
       `https://app.diagrams.net/${url_encoded_data}`,
       "_blank"
     );
     newTab.name = `drawio_tab`;
   }
+
+  if ($(container).attr("devstage") === "true") {
+    openDrawioInNewTab()
+  } else {
+    try {
+      window.parent.postMessage({
+        'func': 'setDrawIO',
+        'message': `${url_encoded_data}`
+      }, "*");  
+    } catch (e) {
+      console.error(e)
+      openDrawioInNewTab()
+    }
+  }
+
   event.preventDefault();
 }
 
